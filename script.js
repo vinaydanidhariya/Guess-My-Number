5d78675376391c54370e3525d8413051193f4453
vinay danidhariya <vinaydanidhariya4114@gmail.com>
2025-02-18 17:29:00 +0530

    #bugFix 2

--- script.js
+++ script.js
@@ -7,70 +7,75 @@ const guess = document.querySelector('.guess');
 const ansMessage = document.querySelector('.ans');
 const againBtn = document.querySelector('.again');
 
-againBtn.addEventListener('onclick', ()=>window.location.reload()) ;
-
+againBtn.addEventListener('onclick', () => window.location.reload());
 
 // 1 to 20
 const ans = Math.round(Math.random() * 20);
 
 // Check event
-const checkEvent = document.querySelector('.check').addEventListener('click', checkNum);
+const checkEvent = document
+  .querySelector('.check')
+  .addEventListener('click', checkNum);
 
 const scoreDecrease = () => {
-    if (Number(score.textContent) <= 1) {
-        alert("LOSE THE GAME");
-    }
-    score.textContent = Number(score.textContent) - 1;
-}
-
-
+  if (Number(score.textContent) <= 1) {
+    alert('LOSE THE GAME');
+  }
+  score.textContent = Number(score.textContent) - 1;
+};
 
 // Set cookie function
 function setCookie(name, value, days) {
-    let expires = "";
-    if (days) {
-        const date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+  let expires = '';
+  if (days) {
+    const date = new Date();
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    expires = '; expires=' + date.toUTCString();
+  }
+  document.cookie = name + '=' + (value || '') + expires + '; path=/';
 }
 
 // Get cookie function
 function getCookie(name) {
-    const cookies = document.cookie.split(';');
-    for (let cookie of cookies) {
-        const [key, value] = cookie.trim().split('=');
-        if (key === name) {
-            return value;
-        }
+  const cookies = document.cookie.split(';');
+  for (let cookie of cookies) {
+    const [key, value] = cookie.trim().split('=');
+    if (key === name) {
+      return value;
     }
-    return null;
+  }
+  return null;
 }
 
-highscore.textContent= getCookie('highscore');
+highscore.textContent = getCookie('highscore');
 
 function checkNum() {
-    let userInput = Number(guess.value);
-    if (Number(score.textContent) <= 0) {
-        alert("LOSE THE GAME PLEASE RESTART GAME");
-        return;
+  if (!guess.value) {
+    alert('PLEASE ENTER NUMBER');
+	return
+  }
+  let userInput = Number(guess.value);
+
+  if (Number(score.textContent) <= 0) {
+    alert('LOSE THE GAME PLEASE RESTART GAME');
+    return;
+  }
+  if (ans === userInput) {
+    message.textContent = 'WINNER';
+    ansMessage.textContent = ans;
+    document.body.setAttribute(
+      'style',
+      'background-color: #60b347; color:white'
+    );
+
+    const highscoreValue = getCookie('highscore');
+    if (Number(score.textContent) > Number(highscoreValue)) {
+      setCookie('highscore', score.textContent, 60);
     }
-    if (ans === userInput) {
-        message.textContent = "WINNER";
-        ansMessage.textContent = ans;
-        document.body.setAttribute("style", "background-color: #60b347; color:white");
-        
-        const highscoreValue = getCookie("highscore");
-        if (Number(score.textContent) > Number(highscoreValue)) {
-            setCookie("highscore", score.textContent, 60);
-        }
-    } else if (ans > userInput) {
-        message.textContent = 'Input is too low';
-        console.log('Input is too low');
-    } else {
-        message.textContent = 'Input is too high';
-        console.log('Input is too high');
-    }
-    scoreDecrease();
+  } else if (ans > userInput) {
+    message.textContent = 'Input is too low';
+  } else {
+    message.textContent = 'Input is too high';
+  }
+  scoreDecrease();
 }
